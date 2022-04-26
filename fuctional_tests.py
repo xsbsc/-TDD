b99a5dce4013f39b532dbac44c53313b6eaf46d2
xsbsc <1249171883@qq.com>
2022-04-26 13:28:01 +0800

    Functional test now checks we can input a to-do item

--- fuctional_tests.py
+++ fuctional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import unittest
-
+from selenium.webdriver.common.by import By
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -13,18 +13,38 @@ class NewVisitorTest(unittest.TestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # #to check out its homepage
         self.browser.get('http://localhost:8000')
-     # She notices the page title and header mention to-do lists
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text =self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        
+
+        # She is invited to enter a to-do item straight away
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+             inputbox.get_Attribute('placeholder'),
+             'Enter a to-do item'
+             )
+        # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        # when she hits enter，the page updates，and now the page lists
+        # #"1:Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+             any(row.text== '1: Buy peacock feathers' for row in rows)
+             )
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         self.fail('Finish the test!')
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-# She is invited to enter a to-do item straight away
-# She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
-# when she hits enter，the page updates，and now the page lists#"1:Buy peacock feathers" as an item in a to-do list
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 # The page updates again，and now shows both items on her list
 # Edith wonders whether the site will remember her list. Then she sees# that the site has generated a unique URL for her -- there is some
 # #explanatory text to that effect.
 # she visits that URL - her to-do list is still there.
 # # Satisfied，she goes back to sleep
+if __name__ == '__main__':
+            unittest.main(warnings='ignore')
