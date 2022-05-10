bfef13ce1332fd8a538bd3de20b4d86850538fc9
xsbsc <1249171883@qq.com>
2022-05-10 08:20:21 +0800

    Front page HTML now generated from a template

--- fuctional_tests.py
+++ fuctional_tests.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time 
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -23,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-             inputbox.get_Attribute('placeholder'),
+             inputbox.get_attribute('placeholder'),
              'Enter a to-do item'
              )
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
@@ -36,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-             any(row.text== '1: Buy peacock feathers' for row in rows)
+             any(row.text== '1: Buy peacock feathers' for row in rows),
+             "New to-do item did not appear in table"
              )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
